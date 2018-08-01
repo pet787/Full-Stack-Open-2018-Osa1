88a9042ad35dea0654959ee06875c98a12cfa04a
pet787 <41019198+pet787@users.noreply.github.com>
2018-08-01 13:39:17 +0300

    1.3

--- index.js
+++ index.js
@@ -37,17 +37,23 @@ const Yhteensa = (props) => {
 
 const App = () => {
   const kurssi = 'Half Stack -sovelluskehitys'
-  const osa1 = 'Reactin perusteet'
-  const tehtavia1 = 10
-  const osa2 = 'Tiedonvälitys propseilla'
-  const tehtavia2 = 7
-  const osa3 = 'Komponenttien tila'
-  const tehtavia3 = 14
+  const osa1 = {
+    nimi: 'Reactin perusteet',
+    tehtavia: 10
+  }
+  const osa2 = {
+    nimi: 'Tiedonvälitys propseilla',
+    tehtavia: 7
+  }
+  const osa3 = {
+    nimi: 'Komponenttien tila',
+    tehtavia: 14
+  }
   return (
     <div>
       <Otsikko k={kurssi} />
-      <Sisalto o1={osa1} t1={tehtavia1} o2={osa2} t2={tehtavia2} o3={osa3} t3={tehtavia3} />
-      <Yhteensa t={tehtavia1 + tehtavia2 + tehtavia3} />
+      <Sisalto o1={osa1.nimi} t1={osa1.tehtavia} o2={osa2.nimi} t2={osa2.tehtavia} o3={osa3.nimi} t3={osa3.tehtavia} />
+      <Yhteensa t={osa1.tehtavia + osa2.tehtavia + osa3.tehtavia} />
     </div> 
   )
 }
